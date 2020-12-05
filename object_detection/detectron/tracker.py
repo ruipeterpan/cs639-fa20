64b3b67de041aa1e054d20ea40c6ea0d689f74ce
ruipeterpan <rpan0726@gmail.com>
2020-12-05 14:21:46 -0600

    Replace Camshift with Meanshift

--- object_detection/detectron/tracker.py
+++ object_detection/detectron/tracker.py
@@ -7,14 +7,19 @@ class Tracker:
 
     def __init__(self):
         """Initialize tracker members"""
-        self.x, self.y, self.width, self.height = 0, 0, 0, 0
-        self.track_window = None
-        self.roi = None
-        self.hsv_roi = None
-        self.mask = None
-        self.roi_hist = None
-        self.term_crit = None
-        self.isFirstIter = True
+        self.x, self.y, self.width, self.height = 600,200,100,300
+        self.track_window = (self.x, self.y, self.width, self.height)
+        self.roi = cv2.imread("./template.png")
+        self.hsv_roi = cv2.cvtColor(self.roi, cv2.COLOR_BGR2HSV) 
+        self.mask = cv2.inRange(self.hsv_roi,  
+                  np.array((0., 60., 40.)), 
+                  np.array((180., 255., 255))) 
+        self.roi_hist = cv2.calcHist([self.hsv_roi],  
+                       [0],self.mask, 
+                       [180],  
+                       [0, 180]) 
+        self.term_crit = ( cv2.TERM_CRITERIA_EPS |  
+             cv2.TERM_CRITERIA_COUNT, 15, 2) 
         self.path_to_src = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -26,68 +31,17 @@ class Tracker:
         Returns:
             numpy.ndarray: An np array of the same size with the desired object tagged
         """
-        frame = np.float32(frame)  # transform frame from int array to float32
-
-        if self.isFirstIter:
-            # initialization, set initial tracking region & calculate histogram
-            self.isFirstIter = False
-
-            # initialize the track_window with random values
-            # initial random values needs to be somewhat close to the actual roi captured in the first frame
-            self.x, self.y, self.width, self.height = 500, 300, 200, 200
-            self.track_window = (self.x, self.y, self.width, self.height)
-
-            # self.roi = frame[y:y+height, x:x+width]
-            self.roi = cv2.imread(self.path_to_src + "/imgs/test.png")
-            # print(self.roi)
-            # convert ROI from BGR to
-            # HSV format
-            self.hsv_roi = cv2.cvtColor(self.roi, cv2.COLOR_BGR2HSV)
-
-            # perform masking operation
-            self.mask = cv2.inRange(
-                self.hsv_roi, np.array((0.0, 60.0, 40.0)), np.array((180.0, 255.0, 255))
-            )
-
-            self.roi_hist = cv2.calcHist(
-                [self.hsv_roi], [0], self.mask, [180], [0, 180]
-            )
-
-            cv2.normalize(self.roi_hist, self.roi_hist, 0, 255, cv2.NORM_MINMAX)
-
-            # Setup the termination criteria,
-            # either 15 iteration or move by
-            # atleast 2 pt
-            self.term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 15, 2)
-
-        _, frame1 = cv2.threshold(frame, 180, 155, cv2.THRESH_TOZERO_INV)
-
-        # convert from BGR to HSV
-        # format.
-        hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
-
-        dst = cv2.calcBackProject([hsv], [0], self.roi_hist, [0, 180], 1)
-
-        # apply Camshift to get the
-        # new location            self.dst, self.track_window, self.term_crit
-
-        ret2, self.track_window = cv2.CamShift(
-            dst, self.track_window, self.term_crit
-        )
-
+        # frame = np.float32(frame)  # transform frame from int array to float32
+        # frame = frame.astype(np.float32)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv],[0],self.roi_hist,[0,180],1)
+        # apply meanshift to get the new location
+        ret, self.track_window = cv2.meanShift(dst, self.track_window, self.term_crit)
         # Draw it on image
-        pts = cv2.boxPoints(
-            ret2
-        )  # something like ((200.0, 472.0), (200.0, 228.0), (420.0, 228.0), (420.0, 472.0))
+        self.x, self.y, self.width, self.height = self.track_window
+        img2 = cv2.rectangle(frame, (self.x,self.y), (self.x+self.width,self.y+self.height), 255,2)
+        cv2.imshow('img2',img2)
+        cv2.imwrite('./test.png',img2)
 
-        # convert from floating
-        # to integer
-        pts = np.int0(pts)
+        return self.track_window, img2
 
-        # Draw Tracking window on the
-        # video frame.
-        output = cv2.polylines(frame, [pts], True, (0, 255, 255), 10)
-
-        cv2.imwrite('./test.png', output)
-
-        return pts, output
