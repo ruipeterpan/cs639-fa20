e9491316c8ada45ed5270fb5cba7efe9a3913c16
Hashiyaa <haochen213300@gmail.com>
2020-12-05 15:24:14 -0600

    Solve an issue about template matching

--- object_detection/detectron/tracker.py
+++ object_detection/detectron/tracker.py
@@ -7,9 +7,10 @@ class Tracker:
 
     def __init__(self):
         """Initialize tracker members"""
-        self.x, self.y, self.width, self.height = 600,200,100,300
+        self.path_to_src = os.path.dirname(os.path.realpath(__file__))
+        self.x, self.y, self.width, self.height = 300,180,50,120
         self.track_window = (self.x, self.y, self.width, self.height)
-        self.roi = cv2.imread("./template.png")
+        self.roi = cv2.imread(self.path_to_src + "/imgs/template.png")
         self.hsv_roi = cv2.cvtColor(self.roi, cv2.COLOR_BGR2HSV) 
         self.mask = cv2.inRange(self.hsv_roi,  
                   np.array((0., 60., 40.)), 
@@ -20,8 +21,6 @@ class Tracker:
                        [0, 180]) 
         self.term_crit = ( cv2.TERM_CRITERIA_EPS |  
              cv2.TERM_CRITERIA_COUNT, 15, 2) 
-        self.path_to_src = os.path.dirname(os.path.realpath(__file__))
-
 
     def track(self, frame):
         """Takes in an input frame and outputs a frame with the desired object
@@ -31,8 +30,10 @@ class Tracker:
         Returns:
             numpy.ndarray: An np array of the same size with the desired object tagged
         """
-        # frame = np.float32(frame)  # transform frame from int array to float32
+        frame = np.uint8(frame)  # transform frame from int array to float32
+        print(frame.dtype)
         # frame = frame.astype(np.float32)
+        print(frame.shape)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         dst = cv2.calcBackProject([hsv],[0],self.roi_hist,[0,180],1)
         # apply meanshift to get the new location
@@ -40,7 +41,7 @@ class Tracker:
         # Draw it on image
         self.x, self.y, self.width, self.height = self.track_window
         img2 = cv2.rectangle(frame, (self.x,self.y), (self.x+self.width,self.y+self.height), 255,2)
-        cv2.imshow('img2',img2)
+        # cv2.imshow('img2',img2)
         cv2.imwrite('./test.png',img2)
 
         return self.track_window, img2
